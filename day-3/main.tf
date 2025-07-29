327b6e5e3e21e5e8c002ea1b767579b02660376b
Santhoish-Kumar-1812 <santhoshkumarkatroth1812@gmail.com>
2025-07-29 14:31:17 +0530

    hello.tfvars

--- day-3/main.tf
+++ day-3/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  
 
   tags = {
-    Name = "AMMA"
+    Name = "ExampleInstance"
   }
   
 }
